b116469a142c0230d0e9fa533f63ddff2928c631
Abbas Srour <abbas.mj.srour@gmail.com>
2022-08-04 11:42:48 +0300

    Added schemas and minxins

---
+++ frontend/src/schemas/movie.schema.ts
@@ -0,0 +1,15 @@
+type ShowSchema = {
+  background: string;
+  title: string;
+  overview: string;
+  poster: string;
+  trailer: string;
+  link: string;
+  length: string;
+  releaseYear: number;
+  pgRating: string;
+  genre: string;
+  cover: string;
+};
+
+export default ShowSchema;
