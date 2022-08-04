38cc2b76e33849bf32e36de56324a91c6ee71ebe
Abbas Srour <abbas.mj.srour@gmail.com>
2022-08-04 11:41:46 +0300

    Added ShowBox

--- frontend/src/components/ShowBox/ShowBox.component.tsx
+++ frontend/src/components/ShowBox/ShowBox.component.tsx
@@ -9,38 +9,18 @@ type Props = {
 
 const ShowBox: React.FC<Props> = ({ width, show }) => {
   const [isHovered, setIsHovered] = useState(false);
-
   return (
     <div
       className="show-box"
-      style={{
-        width: `${width}%`,
-        // position: `${isHovered ? "relative" : "relative"}`,
+      style={{ width: `${width}%` }}
+      onMouseEnter={() => {
+        setTimeout(() => setIsHovered(true), 1000);
       }}
-      onMouseEnter={() => setIsHovered(true)}
+      // onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <img src={show.cover} alt="poster" />
-      {/*    {isHovered && (*/}
-      {/*      <>*/}
-      {/*        <video src={show.trailer} autoPlay={true} loop />*/}
-      {/*        <div className="itemInfo">*/}
-      {/*          <div className="icons">*/}
-      {/*            <PlayArrow className="icon" />*/}
-      {/*            <Add className="icon" />*/}
-      {/*            <ThumbUpAltOutlined className="icon" />*/}
-      {/*            <ThumbDownOutlined className="icon" />*/}
-      {/*          </div>*/}
-      {/*          <div className="itemInfoTop">*/}
-      {/*            <span>{show.length}</span>*/}
-      {/*            <span className="limit">{show.pgRating}</span>*/}
-      {/*            <span>{show.releaseYear}</span>*/}
-      {/*          </div>*/}
-      {/*          <div className="desc">{show.overview}</div>*/}
-      {/*          <div className="genre">{show.genre}</div>*/}
-      {/*        </div>*/}
-      {/*      </>*/}
-      {/*    )}*/}
+      {isHovered && <video src={show.trailer} autoPlay={true} loop />}
     </div>
   );
 };
